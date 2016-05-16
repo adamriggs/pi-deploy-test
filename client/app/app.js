fd047b47f2407e167826965e8cc33ac9301f0229
Adam Riggs <phlux1111@gmail.com>
2016-05-16 15:38:23 -0400

    one more time

---
+++ client/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myAppApp', ['myAppApp.auth', 'myAppApp.admin', 'myAppApp.constants', 'ngCookies',
+    'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'ui.bootstrap',
+    'validation.match'
+  ])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('myAppApp')
+  .config(function($stateProvider) {
+    $stateProvider.state('main', {
+      url: '/',
+      template: '<main></main>'
+    });
+  });
